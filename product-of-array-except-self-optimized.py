a068f3b361f5406efac36249af261ec6f26f123f
Mustafa Aydogdu <aydogdu.mustafa@metu.edu.tr>
2023-04-06 16:56:07 +0300

    Add comments to solution

--- product-of-array-except-self-optimized.py
+++ product-of-array-except-self-optimized.py
@@ -1,15 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
+        # store the result in the result array
         result = [1] * len(nums)
 
-        prefix = 1
+        # first pass to calculate the prefix
+        prefix = 1  # 
         for i in range(1, len(nums)):
             prefix *= nums[i-1]
             result[i] = prefix
 
+        # second pass to calculate the postfix
         postfix = 1
         for i in range(len(nums)-2, -1, -1):
-            postfix *= nums[i+1]
+            postfix *= nums[i+1]  # 
             result[i] *= postfix
 
         return result
