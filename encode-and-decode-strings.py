513c3fab99d4162ab4c5ab95e2ea45c40421f63b
Mustafa Aydogdu <aydogdu.mustafa@metu.edu.tr>
2023-04-09 22:43:09 +0300

    Add Encode and Decode Strings solution

---
+++ encode-and-decode-strings.py
@@ -0,0 +1,35 @@
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # to encode, add the length of the string to the front of the string
+        result = ''
+        for string in strs:
+            result += str(len(string)) + '#' + string
+        
+        return result
+
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, string):
+        result = []
+        i = 0
+
+        while(i < len(string)):
+            j = i
+            while(string[j] != '#'):
+                j += 1
+            length = int(string[i:j])
+            result.append(string[j+1:j+1+length])
+            i = j + 1 + length
+            
+        return result
+        
+
+
+a = Solution()
+print(a.decode(a.encode(['10#adfdafadfl982#a4bcd', '7#d3*(&(e'])))
