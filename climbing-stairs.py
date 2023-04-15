7d2f6afa851bf5323c4aa9f79ecf1f77418c4d4b
Mustafa Aydogdu <aydogdu.mustafa@metu.edu.tr>
2023-04-15 17:16:00 +0300

    Add bottom-up dp solution

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -3,7 +3,13 @@ class Solution:
         if n <= 2:
             return n
 
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
     
 a = Solution()
 print(a.climbStairs(5))
